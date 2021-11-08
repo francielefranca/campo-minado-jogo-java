d270fda37e94bbaf3c0a9c6ab7e24ad72c9fa062
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2021-11-08 19:39:53 -0300

    Update Jogo.java

--- CampoMinadoJogo/src/campoMinado/Jogo.java
+++ CampoMinadoJogo/src/campoMinado/Jogo.java
@@ -6,8 +6,6 @@ public class Jogo {
 	
 	Scanner input = new Scanner(System.in);
 	
-	int quantidadeDeBandeiras = 0;
-	
 	int x, y;
 	char acao;
 	
@@ -17,47 +15,79 @@ public class Jogo {
 	
 	public void pontuacao() {
 		if (tabuleiro.tabuleiro[(-(x - 9))][y] == 'A') {
-			tabuleiro.pontos = tabuleiro.pontos + 10;
+			tabuleiro.pontos = tabuleiro.pontos + 100;
+		}
+	}
+	
+	public void casaAberta() {
+		System.out.println("Casa já aberta.\n");
+	}
+	
+	public void abrirCasa() {
+		if (tabuleiro.tabuleiro[(-(x - 9))][y] == 'B') {
+			System.out.println("Casa ocupada por bandeira!\n");
+		} else {
+			tabuleiro.tabuleiro[(-(x - 9))][y] = 'A';
+			pontuacao();
+			tabuleiro.marcarTabuleiro();
+			System.out.println("");
+		}
+	}
+	
+	public void casa() {
+		if (tabuleiro.tabuleiro[(-(x - 9))][y] == 'A') {
+			casaAberta();
+		} else { 
+			if (tabuleiro.celulas[(-(x - 9))][y] == 7) {
+				abrirCasa();
+			}
 		}
 	}
 	
 	public void contadorDeBandeiras() {
 		if (tabuleiro.tabuleiro[(-(x - 9))][y] == 'B') {
-			++quantidadeDeBandeiras;
-			}
-	}
-	
-	public void abrirCelula() {
-		if (tabuleiro.celulas[(-(x - 9))][y] == 7) {
-			tabuleiro.tabuleiro[(-(x - 9))][y] = 'A';
-			pontuacao();
-			tabuleiro.marcarTabuleiro();
-			System.out.println("");
+			--tabuleiro.quantidadeDeBandeiras;
 			}
 	}
 	
 	public void plantarBandeira() {
 		tabuleiro.tabuleiro[(-(x - 9))][y] = 'B';
 		contadorDeBandeiras();
-		if (quantidadeDeBandeiras > 10) {
-			System.out.println("Suas bandeiras acabaram");
-			acoes();
-		}
 		tabuleiro.marcarTabuleiro();
 		System.out.println("");
 	}
 	
-	public void acoes() {
-		System.out.println("Você quer:\nAbrir a célula (A)\nPlantar uma bandeira (B)");
-		acao = input.next().charAt(0);
-		if (acao == 'a' || acao == 'A') {
-			abrirCelula();
+	public void removerBandeira() {
+		tabuleiro.tabuleiro[(-(x - 9))][y] = '-';
+		++tabuleiro.quantidadeDeBandeiras;
+		tabuleiro.marcarTabuleiro();
+		System.out.println("");
+	}
+	
+	public void bandeira() {
+		if (tabuleiro.tabuleiro[(-(x - 9))][y] != 'B') {
+			plantarBandeira();
+		} else {
+			removerBandeira();
 		}
 		
-		if (acao == 'b' || acao == 'B') {
-			plantarBandeira();
+		if (tabuleiro.quantidadeDeBandeiras < 1) {
+			System.out.println("Suas bandeiras acabaram");
+			acoes();
 		}
 	}
+	
+	public void acoes() {
+		System.out.println("Você quer:\nAbrir casa (A)\nPlantar uma bandeira (B)");
+		acao = input.next().charAt(0);
+		if (acao == 'a' || acao == 'A') {
+			casa();
+		} else if (acao == 'b' || acao == 'B') {
+			bandeira();
+			} else {
+				acoes();
+				}
+		}
 	
 	public void receberCoordenadas() {
 		System.out.print("Insira sua linha: ");
@@ -70,19 +100,22 @@ public class Jogo {
 			receberCoordenadas();
 		}
 	}
-	
-	public void fim() {
-		System.out.println("");
-		for (int i = 1; i <= 8; i++) {
-			System.out.print("            ");
-			for (int j = 1; j <= 8; j++) {
+		
+	public void fimDeJogo() {
+		System.out.println("\n       Linhas\n");
+		for (int i = 1; i < 9; i++) {
+			System.out.print("       " + (-(i - 9)) + " ");
+			for (int j = 1; j < 9; j++) {
 				if (tabuleiro.celulas[i][j] == 0) {
 					tabuleiro.tabuleiro[i][j] = 'X';
 				}
-				System.out.print(tabuleiro.tabuleiro[i][j] + "   ");
+				System.out.print("   " + tabuleiro.tabuleiro[i][j]);
 			}
 			System.out.println("");
 		}
+		System.out.println("\n            1   2   3   4   5   6   7   8");
+        System.out.println("\n				  Colunas");
+		
 		System.out.println("");
 		System.out.println("Havia uma mina, você perdeu.");
 		System.out.println("Pontuação total: " + tabuleiro.pontos);
@@ -94,7 +127,7 @@ public class Jogo {
 			acoes();
 			if (tabuleiro.celulas[(-(x - 9))][y] == 0) {
 				gameOver = true;
-				fim();
+				fimDeJogo();
 				}
 		} while (!gameOver);
 	}
