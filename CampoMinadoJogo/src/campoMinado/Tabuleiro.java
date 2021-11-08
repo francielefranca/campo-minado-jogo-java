e88d1c5c97f20a5cdf156e814a578f12830aca79
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2021-11-07 23:23:30 -0300

    Update Tabuleiro.java

--- CampoMinadoJogo/src/campoMinado/Tabuleiro.java
+++ CampoMinadoJogo/src/campoMinado/Tabuleiro.java
@@ -1,7 +1,52 @@
 package campoMinado;
 
-public class Tabuleiro {
-	int quantidadeBomba = 10;
-	int i, j;
-
+public class Tabuleiro extends Celula {
+	
+	char[][] tabuleiro;
+	
+	public void imprimirTabuleiro() {
+		
+		tabuleiro = new char[9][9];
+		
+		System.out.println("\n       Linhas\n");
+		for (linha = 1; linha < 9; linha++) {
+			System.out.print("       " + (-(linha - 9)) + " ");
+			for (coluna = 1; coluna < 9; coluna++) {
+				tabuleiro[linha][coluna] = '-';
+				System.out.print("   "+ tabuleiro[linha][coluna]);
+			}
+			System.out.println("");
+		}
+		System.out.println("\n            1   2   3   4   5   6   7   8");
+        System.out.println("\n				  Colunas");
+        
+		System.out.println("");
+		System.out.println("Bombas: " + quantidadeDeBombas);
+		System.out.print("Pontos: " + pontos);
+		System.out.println("");
+	}
+	
+	public void marcarTabuleiro() {
+		System.out.println("\n       Linhas\n");
+		for (linha = 1; linha < 9; linha++) {
+			System.out.print("       " + (-(linha - 9)) + " ");
+			for (coluna = 1; coluna < 9; coluna++) {
+				System.out.print("   " + tabuleiro[linha][coluna]);
+			}
+			System.out.println("");
+		}
+		System.out.println("\n            1   2   3   4   5   6   7   8");
+        System.out.println("\n				  Colunas");
+        
+		System.out.println("");
+		System.out.println("Bombas: " + quantidadeDeBombas);
+		System.out.print("Pontos: " + pontos);
+		System.out.println("");
+	}
+	
+	public Tabuleiro() {
+		sortearMinas();
+		imprimirMinas();
+		imprimirTabuleiro();
+	}
 }
