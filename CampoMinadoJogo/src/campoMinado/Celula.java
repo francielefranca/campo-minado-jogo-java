ab105a43bdf46c45103e53ae5918b07d8dfd3d78
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2021-11-07 23:20:37 -0300

    Update Celula.java

--- CampoMinadoJogo/src/campoMinado/Celula.java
+++ CampoMinadoJogo/src/campoMinado/Celula.java
@@ -1,8 +1,51 @@
 package campoMinado;
 
+import java.util.Random;
+
 public class Celula {
-	int i, j;
-	int celulaVazia;
-	int celulaBomba;
-	int celulaNumero;
+	
+	Random random = new Random();
+	
+	int linha;
+	int coluna;
+	
+	int[][] celulas;
+	
+	int numero;
+	
+	int quantidadeDeBombas = 0;
+	
+	int pontos = 0;
+	
+	public void sortearMinas() {
+		int aleatorio = random.nextInt(8);
+		if (aleatorio == 0 || aleatorio == 2) {
+			numero = 0;
+		} else {
+			numero = 7;
+			}
+	}
+	
+	public void contadorDeBombas() {
+		if (numero == 0) {
+			++quantidadeDeBombas;
+			} 
+	}
+	
+	public void imprimirMinas() {
+		
+		celulas = new int[9][9];
+		
+		for (linha = 1; linha <= 8; linha++) {
+			for (coluna = 1; coluna <= 8; coluna++) {
+				if(quantidadeDeBombas < 10) {
+					sortearMinas();
+					contadorDeBombas();
+				} else {
+					numero = 7;
+				}
+				celulas[linha][coluna] = numero;
+			}
+		}
+	}
 }
