963d68633bf61aeb3765e0d3bafd31694cfdc2a5
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2021-11-18 17:59:17 -0300

    Herança da interação com usuário

--- CampoMinadoJogo/src/campoMinado/Jogo.java
+++ CampoMinadoJogo/src/campoMinado/Jogo.java
@@ -1,73 +1,49 @@
 package campoMinado;
 
-import java.util.Scanner;
-
-import java.time.Instant;
-//import java.time.Duration;
-/*import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;*/
-
-//import's para o tempo
-
-public class Jogo {
+public class Jogo extends InteracaoComUsuario {
 	
-	Scanner input = new Scanner(System.in);
+	int y, x;
 	
-	int w, y;
-	int x;
-	
-	char acao;
 	int bombasVizinhas;
 	
 	boolean gameOver;
 	
-	boolean temBomba = false;
-	
-	Tabuleiro tabuleiro = new Tabuleiro();
-	//InteracaoComJogador interacaoComJogador = new InteracaoComJogador();
-	
-	Instant start = Instant.now();//contagem de tempo
+	Tabuleiro casas = new Tabuleiro();
+	Celula minas = new Celula();
 	
 	public void conferirVizinhas() {
 		bombasVizinhas = 0;
 		
-		if ((x - 1) > 0 && tabuleiro.celulas[x - 1][y] == 0) {
+		if ((x - 1) > 0 && minas.celulas[x - 1][y] == 0) {
 			++bombasVizinhas;
 			}
-		if ((x + 1) < 9 && tabuleiro.celulas[x + 1][y] == 0) {
+		if ((x + 1) < 9 && minas.celulas[x + 1][y] == 0) {
 			++bombasVizinhas;
 			} 
-		if ((y + 1) < 9 && tabuleiro.celulas[x][y + 1] == 0) {
+		if ((y + 1) < 9 && minas.celulas[x][y + 1] == 0) {
 			++bombasVizinhas;
 			} 
-		if ((y - 1) > 0 && tabuleiro.celulas[x][y - 1] == 0) {
+		if ((y - 1) > 0 && minas.celulas[x][y - 1] == 0) {
 			++bombasVizinhas;
 			} 
-		if (((x + 1) < 9 && (y + 1) < 9) && tabuleiro.celulas[x + 1][y + 1] == 0) {
+		if (((x + 1) < 9 && (y + 1) < 9) && minas.celulas[x + 1][y + 1] == 0) {
 			++bombasVizinhas;
 			} 
-		if (((x - 1) > 0 && (y - 1) > 0) && tabuleiro.celulas[x - 1][y - 1] == 0) {
+		if (((x - 1) > 0 && (y - 1) > 0) && minas.celulas[x - 1][y - 1] == 0) {
 			++bombasVizinhas;
 			}
-		if (((x + 1) < 9 && (y - 1) > 0) && tabuleiro.celulas[x + 1][y - 1] == 0) {
+		if (((x + 1) < 9 && (y - 1) > 0) && minas.celulas[x + 1][y - 1] == 0) {
 			++bombasVizinhas;
 			}
-		if (((x - 1) > 0 && (y + 1) < 9) && tabuleiro.celulas[x - 1][y + 1] == 0) {
+		if (((x - 1) > 0 && (y + 1) < 9) && minas.celulas[x - 1][y + 1] == 0) {
 			++bombasVizinhas;
 			}
-		tabuleiro.tabuleiro[x][y] = Character.forDigit(bombasVizinhas, 10);
+		casas.tabuleiro[x][y] = Character.forDigit(bombasVizinhas, 10);
     }
 	
-	/*
-	public void abrirCasaVizinha() {
-	   if(tabuleiro.tabuleiro[x][y] == true) {
-		for()
-	   }
-	}*/
-	
 	public void pontuacao() {
-		if (tabuleiro.celulas[x][y] == 1) {
-			tabuleiro.pontos = tabuleiro.pontos + 100;
+		if (minas.celulas[x][y] == 1) {
+			minas.pontos = minas.pontos + 100;
 		}
 	}
 	
@@ -76,83 +52,76 @@ public class Jogo {
 	}
 	
 	public void abrirCasa() {
-		if (tabuleiro.tabuleiro[x][y] == 'B') {
+		if (casas.tabuleiro[x][y] == 'B') {
 			System.out.println("Casa ocupada por bandeira!\n");
 		} else {
 			conferirVizinhas();
 			pontuacao();
-			tabuleiro.marcarTabuleiro();
+			casas.marcarTabuleiro();
 			System.out.println("");
 		}
 	}
 	
 	public void casa() {
-		if (tabuleiro.tabuleiro[(x)][y] != '-') {
+		if (casas.tabuleiro[(x)][y] != '-') {
 			casaAberta();
 		} else { 
-			if (tabuleiro.celulas[(x)][y] == 1) {
+			if (minas.celulas[(x)][y] == 1) {
 				abrirCasa();
 			}
 		}
 	}
 	
 	public void contadorDeBandeiras() {
-		if (tabuleiro.tabuleiro[x][y] == 'B') {
-			--tabuleiro.quantidadeDeBandeiras;
+		if (casas.tabuleiro[x][y] == 'B') {
+			--minas.quantidadeDeBandeiras;
 			}
 	}
 	
 	public void plantarBandeira() {
-		tabuleiro.tabuleiro[x][y] = 'B';
+		casas.tabuleiro[x][y] = 'B';
 		contadorDeBandeiras();
-		tabuleiro.marcarTabuleiro();
+		casas.marcarTabuleiro();
 		System.out.println("");
 	}
 	
 	public void removerBandeira() {
-		tabuleiro.tabuleiro[x][y] = '-';
-		++tabuleiro.quantidadeDeBandeiras;
-		tabuleiro.marcarTabuleiro();
+		casas.tabuleiro[x][y] = '-';
+		++minas.quantidadeDeBandeiras;
+		casas.marcarTabuleiro();
 		System.out.println("");
 	}
 	
 	public void bandeira() {
-		if (tabuleiro.tabuleiro[x][y] != 'B') {
+		if (casas.tabuleiro[x][y] != 'B') {
 			plantarBandeira();
 		} else {
 			removerBandeira();
 		}
 		
-		if (tabuleiro.quantidadeDeBandeiras < 1) {
+		if (minas.quantidadeDeBandeiras < 1) {
 			System.out.println("Suas bandeiras acabaram");
-			//interacaoComJogador.acoes();
 			acoes();
 		}
 	}
 	
-	public void acoes() { //nao eh o ideal ficar aqui, se possivel em interacao com o usuario
+	public void acoes() {
 		System.out.println("Você quer:\nAbrir casa (A)\nPlantar uma bandeira (B)");
-		acao = input.next().charAt(0);
-		if (acao == 'a' || acao == 'A') {
+		setAcao();
+		if (getAcao() == 'a' || getAcao() == 'A') {
 			casa();
-		} else if (acao == 'b' || acao == 'B') {
+		} else {
 			bandeira();
-			} else {
-				acoes();
-				}
+			}
 		}
 	
-	public void receberCoordenadas() { //nao eh o ideal ficar aqui, se possivel em interacao com o usuario
+	public void receberCoordenadas() {
 		System.out.print("Insira sua linha: ");
-		w = input.nextInt();
-		x = (-(w - 9));
-		
+		setLinha();
+		x = (-(getLinha() - 9));
 		System.out.print("Insira sua coluna: ");
-		y = input.nextInt();
-		
-		if (x > 8 || y > 8 || x < 1 || y < 1) {
-			receberCoordenadas();
-		}
+		setColuna();
+		y = getColuna();
 	}
 		
 	public void fimDeJogo() {
@@ -160,10 +129,10 @@ public class Jogo {
 		for (int i = 1; i < 9; i++) {
 			System.out.print("       " + (-(i - 9)) + " ");
 			for (int j = 1; j < 9; j++) {
-				if (tabuleiro.celulas[i][j] == 0) {
-					tabuleiro.tabuleiro[i][j] = 'X';
+				if (minas.celulas[i][j] == 0) {
+					casas.tabuleiro[i][j] = 'X';
 				}
-				System.out.print("   " + tabuleiro.tabuleiro[i][j]);
+				System.out.print("   " + casas.tabuleiro[i][j]);
 			}
 			System.out.println("");
 		}
@@ -171,17 +140,15 @@ public class Jogo {
         System.out.println("\n				  Colunas");
 		
 		System.out.println("");
-		System.out.println("Havia uma mina, você perdeu."); //adicionar mensagem para quando vencer tambem
-		System.out.println("Pontuação total: " + tabuleiro.pontos); //pontuacao para o ranking
+		System.out.println("Havia uma mina, você perdeu.");
+		System.out.println("Pontuação total: " + minas.pontos);
 	}
 	
-	public void jogabilidade() {
+	public void jogo() {
 		do {
-			//interacaoComJogador.receberCoordenadas(); - teste com a classe de interacao com o usuario
-			//interacaoComJogador.acoes();
 			receberCoordenadas();
 			acoes();
-			if (tabuleiro.celulas[x][y] == 0) {
+			if (minas.celulas[x][y] == 0) {
 				gameOver = true;
 				fimDeJogo();
 				}
@@ -189,6 +156,6 @@ public class Jogo {
 	}
 
 	public Jogo() {
-		jogabilidade();
+		jogo();
 	}	
 }
