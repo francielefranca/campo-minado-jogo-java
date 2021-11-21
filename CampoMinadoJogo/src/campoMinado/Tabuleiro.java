5eee9aa313311affa6ec4d8bdd188852b4354262
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2021-11-21 18:11:33 -0300

    Pequena correção na marcação

--- CampoMinadoJogo/src/campoMinado/Tabuleiro.java
+++ CampoMinadoJogo/src/campoMinado/Tabuleiro.java
@@ -20,18 +20,20 @@ public class Tabuleiro {
 		System.out.println();
 	}
 	
-	public void marcarTabuleiro() {
-		for (casas.linha = 1; casas.linha < 9; casas.linha++) {
+	public void marcarTabuleiro(boolean gameOver) {
+		if(gameOver == false) {
+			for (casas.linha = 1; casas.linha < 9; casas.linha++) {
 			for (casas.coluna = 1; casas.coluna < 9; casas.coluna++) {
 				System.out.print(casas.celulas[casas.linha][casas.coluna] + "   ");
 				}
-			System.out.println();
+				System.out.println();
 			}
-		System.out.println();
-		System.out.println("Bombas: " +casas.quantidadeDeBombas);
-		System.out.println("Pontos: " +casas.pontos);
-		System.out.println("Bandeiras : " +casas.quantidadeDeBandeiras);
-		System.out.println();
+			System.out.println();
+			System.out.println("Bombas: " +casas.quantidadeDeBombas);
+			System.out.println("Pontos: " +casas.pontos);
+			System.out.println("Bandeiras : " +casas.quantidadeDeBandeiras);
+			System.out.println();
+		}
 	}
 	
 	public Tabuleiro() {
