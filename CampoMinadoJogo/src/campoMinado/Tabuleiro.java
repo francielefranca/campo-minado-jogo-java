89016f06bf8ef8475d53370ea68eabb0262521fa
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2021-11-21 17:40:20 -0300

    Geração do tabuleiro utilizando o tipo célula

--- CampoMinadoJogo/src/campoMinado/Tabuleiro.java
+++ CampoMinadoJogo/src/campoMinado/Tabuleiro.java
@@ -2,52 +2,36 @@ package campoMinado;
 
 public class Tabuleiro {
 	
-	Celula contadores = new Celula();
-	
-	char[][] tabuleiro;
-	
-	int x, y;
+	Celula casas = new Celula();
 	
 	public void imprimirTabuleiro() {
-		
-		tabuleiro = new char[9][9];
-		
-		System.out.println("\n       Linhas\n");
-		for (this.x = 1; this.x < 9; this.x++) {
-			System.out.print("       " + (-(this.x - 9)) + " ");
-			for (this.y = 1; this.y < 9; this.y++) {
-				tabuleiro[x][y] = '-';
-				System.out.print("   "+ tabuleiro[this.x][this.y]);
+		casas.celulas = new int[10][10];
+		for (casas.linha = 1; casas.linha < 9; casas.linha++) {
+			for (casas.coluna = 1; casas.coluna < 9; casas.coluna++) {
+				casas.celulas[casas.linha][casas.coluna] = 9;
+				System.out.print(casas.celulas[casas.linha][casas.coluna] + "   ");
+				}
+			System.out.println();
 			}
-			System.out.println("");
-		}
-		System.out.println("\n            1   2   3   4   5   6   7   8");
-        System.out.println("\n				  Colunas");
-        
-		System.out.println("");
-		System.out.println("Bombas: " + 10);
-		System.out.println("Pontos: " + 0);
-		System.out.println("Bandeiras : " + 10);
-		System.out.println("");
+		System.out.println();
+		System.out.println("Bombas: " +casas.quantidadeDeBombas);
+		System.out.println("Pontos: " +casas.pontos);
+		System.out.println("Bandeiras : " +casas.quantidadeDeBandeiras);
+		System.out.println();
 	}
 	
 	public void marcarTabuleiro() {
-		System.out.println("\n       Linhas\n");
-		for (this.x = 1; this.x < 9; this.x++) {
-			System.out.print("       " + (-(this.x - 9)) + " ");
-			for (this.y = 1; this.y < 9; this.y++) {
-				System.out.print("   " + tabuleiro[this.x][this.y]);
+		for (casas.linha = 1; casas.linha < 9; casas.linha++) {
+			for (casas.coluna = 1; casas.coluna < 9; casas.coluna++) {
+				System.out.print(casas.celulas[casas.linha][casas.coluna] + "   ");
+				}
+			System.out.println();
 			}
-			System.out.println("");
-		}
-		System.out.println("\n            1   2   3   4   5   6   7   8");
-        System.out.println("\n				  Colunas");
-        
-		System.out.println("");
-		System.out.println("Bombas: " + contadores.quantidadeDeBombas);
-		System.out.println("Pontos: " + contadores.pontos);
-		System.out.println("Bandeiras : " + contadores.quantidadeDeBandeiras);
-		System.out.println("");
+		System.out.println();
+		System.out.println("Bombas: " +casas.quantidadeDeBombas);
+		System.out.println("Pontos: " +casas.pontos);
+		System.out.println("Bandeiras : " +casas.quantidadeDeBandeiras);
+		System.out.println();
 	}
 	
 	public Tabuleiro() {
