917ddcff060ceb35e9095492f1869152244c4550
Franciele França <92552849+francielefranca@users.noreply.github.com>
2021-11-22 16:48:41 -0300

    Update Menu.java

--- CampoMinadoJogo/src/campoMinado/Menu.java
+++ CampoMinadoJogo/src/campoMinado/Menu.java
@@ -13,19 +13,16 @@ public class Menu extends InteracaoComUsuario {
 	
 	private void opcoesDoMenu() {
 		
-		System.out.print("\nO que voce deseja acessar?\n" + "1 - Iniciar jogo\n" + "2 - Selecao de dificuldade\n" + "3 - Regras do Campo Minado\n");
+		System.out.print("\nO que voce deseja acessar?\n" + "1 - Iniciar jogo\n" + "2 - Regras do Campo Minado\n");
 		setOpcao();
 		System.out.println("");
 		
 		switch(getOpcao()) {
 		
-		case 1:
+		case '1':
 			new Jogo();
 			break;
-		case 2:
-			dificuldadesDoJogo();
-			break;
-		case 3:
+		case '2':
 			regrasDoJogo();
 			break;
 		default: opcoesDoMenu();
@@ -34,13 +31,7 @@ public class Menu extends InteracaoComUsuario {
 	}
 	
 	private void regrasDoJogo() {
-		System.out.println("O jogo consiste em limpar as areas do tabuleiro sem explodir as minas que estão distribuidas aleatoriamente pelo mesmo. Logo, jogador deve escolher a coordenada(linha, coluna) de onde deseja revelar ou marcar com uma banderia, por exemplo: 2 e 5 (linha 2, coluna 5). Se\nna casa 2 e 5 houver uma mina, GAME OVER!\n");
-		opcoesDoMenu();
-	}
-	
-	private void dificuldadesDoJogo() {
-		System.out.print("Escolha a dificuldade do jogo:\n1 - Facil\n2 - Medio\n3 - Dificil\n");
-		setDificuldade();
+		System.out.println("O jogo consiste em limpar as areas do tabuleiro sem explodir as minas que estao distribuidas aleatoriamente pelo mesmo. Logo, jogador deve escolher a coordenada(linha, coluna) de onde deseja revelar ou marcar com uma banderia, por exemplo: 2 e 5 (linha 2, coluna 5). Se\nna casa 2 e 5 houver uma mina, GAME OVER!\n");
 		opcoesDoMenu();
 	}
 }
