106c1b99041b7a2d3fe331960309cb28414bf68f
Franciele França <92552849+francielefranca@users.noreply.github.com>
2021-11-22 16:50:28 -0300

    Update Tabuleiro.java

--- CampoMinadoJogo/src/campoMinado/Tabuleiro.java
+++ CampoMinadoJogo/src/campoMinado/Tabuleiro.java
@@ -4,10 +4,10 @@ public class Tabuleiro {
 	
 	Celula casas = new Celula();
 	
-	public void imprimirTabuleiro() {
-		casas.celulas = new int[10][10];
-		for (casas.linha = 1; casas.linha < 9; casas.linha++) {
-			for (casas.coluna = 1; casas.coluna < 9; casas.coluna++) {
+	protected void imprimirTabuleiro() {
+		casas.celulas = new int[11][11];
+		for (casas.linha = 1; casas.linha <= 9; casas.linha++) {
+			for (casas.coluna = 1; casas.coluna <= 9; casas.coluna++) {
 				casas.celulas[casas.linha][casas.coluna] = 9;
 				System.out.print(casas.celulas[casas.linha][casas.coluna] + "   ");
 				}
@@ -20,10 +20,10 @@ public class Tabuleiro {
 		System.out.println();
 	}
 	
-	public void marcarTabuleiro(boolean gameOver) {
+	protected void marcarTabuleiro(boolean gameOver) {
 		if(gameOver == false) {
-			for (casas.linha = 1; casas.linha < 9; casas.linha++) {
-			for (casas.coluna = 1; casas.coluna < 9; casas.coluna++) {
+			for (casas.linha = 1; casas.linha <= 9; casas.linha++) {
+			for (casas.coluna = 1; casas.coluna <= 9; casas.coluna++) {
 				System.out.print(casas.celulas[casas.linha][casas.coluna] + "   ");
 				}
 				System.out.println();
