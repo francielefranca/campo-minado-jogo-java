48232035aa8d712e7794ab899aa5778d8e2c48e4
Franciele França <92552849+francielefranca@users.noreply.github.com>
2021-11-24 13:30:09 -0300

    Update Jogo.java

--- CampoMinadoJogo/src/campoMinado/Jogo.java
+++ CampoMinadoJogo/src/campoMinado/Jogo.java
@@ -2,55 +2,47 @@ package campoMinado;
 
 import jogador.InteracaoComUsuario;
 
-public class Jogo extends InteracaoComUsuario {
-	
-	Celula posicao = new Celula();
+public class Jogo extends InteracaoComUsuario {;
+
 	Tabuleiro tabuleiro = new Tabuleiro();
+	Celula casas = new Celula();
 	
-	int y, x;
+	private int x, y;
 	
-	int bombasVizinhas;
+	private int bombasVizinhas;
 	
-	boolean gameOver;
+	protected int pontos = 0;
+	
+	protected boolean gameOver;
 	
 	private void conferirVizinhas() {
-		
 		bombasVizinhas = 0;
-		
-		if ((this.x - 1) > 0 && posicao.celulas[this.x - 1][this.y] == 1) {
-			++this.bombasVizinhas;
+		for(int i = -1; i <= 1; i++) {
+			for(int j = -1; j <= 1; j++) {
+				if(casas.celulas[this.x + i][this.y + j] == 1 && (this.x + i) <= 16 && (this.x - i) > 0 && (this.y + j) <= 16 && (this.y - j) > 0) {
+					++this.bombasVizinhas;
+				}
 			}
-		if ((this.x + 1) < 10 && posicao.celulas[this.x + 1][this.y] == 1) {
-			++this.bombasVizinhas;
-			} 
-		if ((this.y + 1) < 10 && posicao.celulas[this.x][this.y + 1] == 1) {
-			++this.bombasVizinhas;
-			} 
-		if ((this.y - 1) > 0 && posicao.celulas[this.x][this.y - 1] == 1) {
-			++this.bombasVizinhas;
-			} 
-		if (((this.x + 1) < 10 && (this.y + 1) < 10) && posicao.celulas[this.x + 1][this.y + 1] == 1) {
-			++this.bombasVizinhas;
-			} 
-		if (((this.x - 1) > 0 && (this.y - 1) > 0) && posicao.celulas[this.x - 1][this.y - 1] == 1) {
-			++this.bombasVizinhas;
-			}
-		if (((this.x + 1) < posicao.linha + 1 && (this.y - 1) > 0) && posicao.celulas[this.x + 1][this.y - 1] == 1) {
-			++this.bombasVizinhas;
-			}
-		if (((this.x - 1) > 0 && (this.y + 1) < posicao.coluna + 1) && posicao.celulas[this.x - 1][this.y + 1] == 1) {
-			++this.bombasVizinhas;
-			}
+		}
 		tabuleiro.casas.celulas[this.x][this.y] = this.bombasVizinhas;
-		
     }
 	
 	private void pontuacao() {
-		tabuleiro.casas.pontos = tabuleiro.casas.pontos + 100;
+		if(this.gameOver) {
+			return;
+		} else {
+			pontos = pontos + 100;
+		}
 	}
 	
 	private void casaAberta() {
 		System.out.println("Casa já aberta.\n");
+	}
+	
+	private void exibirPontuacao() {
+		if(this.gameOver == false) {
+			System.out.println("Pontuação: " + pontos);
+		}
 	}
 	
 	private void abrirCasa() {
@@ -60,8 +52,6 @@ public class Jogo extends InteracaoComUsuario {
 			conferirGameOver();
 			pontuacao();
 			conferirVizinhas();
-			tabuleiro.marcarTabuleiro(gameOver);
-			System.out.println("");
 		}
 	}
 	
@@ -75,24 +65,20 @@ public class Jogo extends InteracaoComUsuario {
 	
 	private void contadorDeBandeiras() {
 		if (tabuleiro.casas.celulas[this.x][this.y] == 8) {
-			--tabuleiro.casas.quantidadeDeBandeiras;
+			--tabuleiro.quantidadeDeBandeiras;
 			} else {
-				++tabuleiro.casas.quantidadeDeBandeiras;
+				++tabuleiro.quantidadeDeBandeiras;
 			}
 	}
 	
 	private void plantarBandeira() {
 		tabuleiro.casas.celulas[this.x][this.y] = 8;
 		contadorDeBandeiras();
-		tabuleiro.marcarTabuleiro(gameOver);
-		System.out.println("");
 	}
 	
 	private void removerBandeira() {
 		tabuleiro.casas.celulas[this.x][this.y] = 9;
 		contadorDeBandeiras();
-		tabuleiro.marcarTabuleiro(gameOver);
-		System.out.println("");
 	}
 	
 	private void bandeira() {
@@ -102,7 +88,7 @@ public class Jogo extends InteracaoComUsuario {
 			removerBandeira();
 		}
 		
-		if (posicao.quantidadeDeBandeiras < 1) {
+		if (tabuleiro.quantidadeDeBandeiras < 1) {
 			System.out.println("Suas bandeiras acabaram");
 			acoes();
 		}
@@ -111,7 +97,7 @@ public class Jogo extends InteracaoComUsuario {
 	private void acoes() {
 		System.out.println("Você quer:\nAbrir casa (A)\nPlantar uma bandeira (B)");
 		setAcao();
-		if (getAcao() == 'a' || getAcao() == 'A') {
+		if (getAcao() == 'a' | getAcao() == 'A') {
 			casa();
 		} else {
 			bandeira();
@@ -121,44 +107,45 @@ public class Jogo extends InteracaoComUsuario {
 	private void receberCoordenadas() {
 		System.out.print("Insira sua linha: ");
 		setLinha();
-		this.x = (-(getLinha() - 10));
+		this.x = (-(getLinha() - 16));
 		System.out.print("Insira sua coluna: ");
 		setColuna();
 		this.y = getColuna();
 	}
 	
 	private void conferirGameOver() {
-		if(posicao.celulas[this.x][this.y] == 1) {
-			gameOver = true;
+		if(casas.celulas[this.x][this.y] == 1) {
+			this.gameOver = true;
 		}
 	}
 		
 	private void fimDeJogo() {
-		for (int i = 1; i <= 9; i++) {
-			for (int j = 1; j <= 9; j++) {
-				if (posicao.celulas[i][j] == 1) {
+		for (tabuleiro.linha = 1; tabuleiro.linha <= 16 ; tabuleiro.linha++) {
+			for (tabuleiro.coluna= 1; tabuleiro.coluna <= 16; tabuleiro.coluna++) {
+				if (casas.celulas[tabuleiro.linha][tabuleiro.coluna] == 1) {
 				}
-				System.out.print(posicao.celulas[i][j] + "   ");
+				System.out.print(casas.celulas[tabuleiro.linha][tabuleiro.coluna] + "   ");
 			}
 			System.out.println("");
 		}
-		
 		System.out.println("");
 		System.out.println("Havia uma mina, você perdeu.");
-		System.out.println("Pontuação total: " + tabuleiro.casas.pontos);
+		System.out.println("Pontuação total: " + pontos);
 	}
 	
-	private void jogo() {
+	protected void jogo() {
 		do {
 			receberCoordenadas();
 			acoes();
+			tabuleiro.marcarTabuleiro(gameOver);
+			exibirPontuacao();
 			if (this.gameOver) {
 				fimDeJogo();
 				}
 		} while (!this.gameOver);
 	}
-
+	
 	public Jogo() {
 		jogo();
-	}	
+	}
 }
