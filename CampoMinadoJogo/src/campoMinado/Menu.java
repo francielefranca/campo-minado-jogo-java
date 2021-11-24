fbbb635d66d457c3282297b08eee7c22dad37bbd
Franciele França <92552849+francielefranca@users.noreply.github.com>
2021-11-24 13:27:01 -0300

    Update Menu.java

--- CampoMinadoJogo/src/campoMinado/Menu.java
+++ CampoMinadoJogo/src/campoMinado/Menu.java
@@ -2,10 +2,9 @@ package campoMinado;
 
 import jogador.InteracaoComUsuario;
 
-public class Menu extends InteracaoComUsuario {
+public class Menu extends InteracaoComUsuario{
 	
 	public Menu() {
-		
 		System.out.print("Digite o seu nome: ");
 		setNome();
 		opcoesDoMenu();
@@ -13,7 +12,7 @@ public class Menu extends InteracaoComUsuario {
 	
 	private void opcoesDoMenu() {
 		
-		System.out.print("\nO que voce deseja acessar?\n" + "1 - Iniciar jogo\n" + "2 - Regras do Campo Minado\n");
+		System.out.print("\nO que você deseja acessar?\n" + "1 - Iniciar jogo\n" + "2 - Regras do jogo\n");
 		setOpcao();
 		System.out.println("");
 		
@@ -31,7 +30,7 @@ public class Menu extends InteracaoComUsuario {
 	}
 	
 	private void regrasDoJogo() {
-		System.out.println("O jogo consiste em limpar as areas do tabuleiro sem explodir as minas que estao distribuidas aleatoriamente pelo mesmo. Logo, jogador deve escolher a coordenada(linha, coluna) de onde deseja revelar ou marcar com uma banderia, por exemplo: 2 e 5 (linha 2, coluna 5). Se\nna casa 2 e 5 houver uma mina, GAME OVER!\n");
+		System.out.println("O jogo consiste em limpar as áreas do tabuleiro sem explodir as minas que estão distribuídas aleatóriamente pelo mesmo. Logo, jogador deve escolher a coordenada(linha, coluna) de onde deseja revelar ou marcar com uma banderia, por exemplo: 2 e 5 (linha 2, coluna 5). Se\nna casa 2 e 5 houver uma mina, GAME OVER!\n");
 		opcoesDoMenu();
 	}
 }
