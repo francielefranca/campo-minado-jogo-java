d244914b5726f1bb2fdfd059e3e94a64a5f213c6
Franciele França <92552849+francielefranca@users.noreply.github.com>
2021-11-24 13:28:49 -0300

    Update Tabuleiro.java

--- CampoMinadoJogo/src/campoMinado/Tabuleiro.java
+++ CampoMinadoJogo/src/campoMinado/Tabuleiro.java
@@ -1,41 +1,42 @@
 package campoMinado;
 
 public class Tabuleiro {
+		
+	public Celula casas = new Celula();
 	
-	Celula casas = new Celula();
+	protected int linha, coluna;
 	
+	protected int quantidadeDeBombas = 40, quantidadeDeBandeiras = 40;
+		
 	protected void imprimirTabuleiro() {
-		casas.celulas = new int[11][11];
-		for (casas.linha = 1; casas.linha <= 9; casas.linha++) {
-			for (casas.coluna = 1; casas.coluna <= 9; casas.coluna++) {
-				casas.celulas[casas.linha][casas.coluna] = 9;
-				System.out.print(casas.celulas[casas.linha][casas.coluna] + "   ");
+		casas.celulas = new int[18][18];
+		for (linha = 1; linha <= 16 ; linha++) {
+			for (coluna = 1; coluna <= 16; coluna++) {
+				casas.celulas[linha][coluna] = 9;
+				System.out.print(casas.celulas[linha][coluna] + "   ");
 				}
 			System.out.println();
 			}
 		System.out.println();
-		System.out.println("Bombas: " +casas.quantidadeDeBombas);
-		System.out.println("Pontos: " +casas.pontos);
-		System.out.println("Bandeiras : " +casas.quantidadeDeBandeiras);
+		System.out.println("Bombas: " +quantidadeDeBombas);
+		System.out.println("Bandeiras : " +quantidadeDeBandeiras);
 		System.out.println();
 	}
-	
+		
 	protected void marcarTabuleiro(boolean gameOver) {
 		if(gameOver == false) {
-			for (casas.linha = 1; casas.linha <= 9; casas.linha++) {
-			for (casas.coluna = 1; casas.coluna <= 9; casas.coluna++) {
-				System.out.print(casas.celulas[casas.linha][casas.coluna] + "   ");
+			for (linha = 1; linha <= 16; linha++) {
+				for (coluna= 1; coluna <= 16; coluna++) {
+					System.out.print(casas.celulas[linha][coluna] + "   ");
 				}
 				System.out.println();
 			}
 			System.out.println();
-			System.out.println("Bombas: " +casas.quantidadeDeBombas);
-			System.out.println("Pontos: " +casas.pontos);
-			System.out.println("Bandeiras : " +casas.quantidadeDeBandeiras);
-			System.out.println();
+			System.out.println("Bombas: " +quantidadeDeBombas);
+			System.out.println("Bandeiras : " +quantidadeDeBandeiras);
 		}
 	}
-	
+		
 	public Tabuleiro() {
 		imprimirTabuleiro();
 	}
