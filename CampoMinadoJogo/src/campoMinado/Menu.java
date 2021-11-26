ec9dab5442945ffa0d5fbd7a32d75c75d338b1e2
Franciele França <92552849+francielefranca@users.noreply.github.com>
2021-11-26 07:45:33 -0300

    Adição de comentários

--- CampoMinadoJogo/src/campoMinado/Menu.java
+++ CampoMinadoJogo/src/campoMinado/Menu.java
@@ -10,7 +10,7 @@ public class Menu extends InteracaoComUsuario{
 		opcoesDoMenu();
 	}
 	
-	private void opcoesDoMenu() {
+	private void opcoesDoMenu() {//opcoes do menu do jogo
 		
 		System.out.print("\nO que você deseja acessar?\n" + "1 - Iniciar jogo\n" + "2 - Regras do jogo\n");
 		super.setOpcao();
@@ -29,7 +29,7 @@ public class Menu extends InteracaoComUsuario{
 		}
 	}
 	
-	private void regrasDoJogo() {
+	private void regrasDoJogo() {//explicacao de como o jogo funciona
 		System.out.println("O jogo consiste em limpar as áreas do tabuleiro sem explodir as minas que estão distribuídas aleatóriamente pelo mesmo. Logo, jogador deve escolher a coordenada(linha, coluna) de onde deseja revelar ou marcar com uma banderia, por exemplo: 2 e 5 (linha 2, coluna 5). Se\nna casa 2 e 5 houver uma mina, GAME OVER!\n");
 		opcoesDoMenu();
 	}
