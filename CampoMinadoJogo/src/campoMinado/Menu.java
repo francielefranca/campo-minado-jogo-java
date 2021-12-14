037060bd61b159bdd1dd457552309a05dfe931dd
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2021-12-14 00:54:06 -0300

    Aplicação das exceções e dificuldades

--- CampoMinadoJogo/src/campoMinado/Menu.java
+++ CampoMinadoJogo/src/campoMinado/Menu.java
@@ -1,36 +1,54 @@
 package campoMinado;
 
+import excecoes.OpcaoInvalidaException;
 import jogador.InteracaoComUsuario;
 
-public class Menu extends InteracaoComUsuario{
+public class Menu extends InteracaoComUsuario {
 	
-	public Menu() {
-		System.out.print("Digite o seu nome: ");
+	Jogo iniciar;
+
+	public Menu() throws OpcaoInvalidaException {
 		super.setNome();
 		opcoesDoMenu();
 	}
-	
-	private void opcoesDoMenu() {//opcoes do menu do jogo
-		
-		System.out.print("\nO que você deseja acessar?\n" + "1 - Iniciar jogo\n" + "2 - Regras do jogo\n");
-		super.setOpcao();
-		System.out.println("");
-		
-		switch(super.getOpcao()) {
-		
+
+	private void opcoesDoMenu() throws OpcaoInvalidaException {// opcoes do menu do jogo
+		try{
+			super.setOpcao();
+		} catch(OpcaoInvalidaException opcao) {
+			System.out.println(opcao.getMessage());
+			opcoesDoMenu();
+		}
+		switch (super.getOpcao()) {
 		case '1':
-			new Jogo();
+			try {
+				iniciar = new Jogo(getDificuldade());
+			} catch (NullPointerException nula) {
+				System.out.println("Antes de comecar, escolha uma dificuldade"); 
+				seletorDeDificuldades();
+			}
 			break;
 		case '2':
 			regrasDoJogo();
 			break;
-		default: opcoesDoMenu();
-		
+		case '3':
+			seletorDeDificuldades();
+			break;
 		}
 	}
+
+	private void regrasDoJogo() throws OpcaoInvalidaException {// explicacao de como o jogo funciona
+		System.out.println("O jogo consiste em limpar o tabuleiro sem explodir as minas que estao\ndistribuidas aleatoriamente pelo mesmo. Logo,o jogador deve escolher\na coordenada(linha, coluna) de onde deseja revelar ou marcar com uma\nbanderia, por exemplo: 2 e 5 (linha 2, coluna 5). Se na casa (2,5) houver\numa mina... GAME OVER!");
+		opcoesDoMenu();
+	}
 	
-	private void regrasDoJogo() {//explicacao de como o jogo funciona
-		System.out.println("O jogo consiste em limpar as áreas do tabuleiro sem explodir as minas que estão distribuídas aleatóriamente pelo mesmo. Logo, jogador deve escolher a coordenada(linha, coluna) de onde deseja revelar ou marcar com uma banderia, por exemplo: 2 e 5 (linha 2, coluna 5). Se\nna casa 2 e 5 houver uma mina, GAME OVER!\n");
+	private void seletorDeDificuldades() throws OpcaoInvalidaException {
+		try {
+			super.setDificuldade();
+		} catch(OpcaoInvalidaException dificuldade) {
+			System.out.println(dificuldade.getMessage());
+			seletorDeDificuldades();
+		}
 		opcoesDoMenu();
 	}
 }
