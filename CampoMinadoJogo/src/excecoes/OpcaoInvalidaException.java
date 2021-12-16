f5702d1bb3ec57cd4f450b7743c6f651f9cd1b4a
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2021-12-16 14:06:25 -0300

    Update OpcaoInvalidaException.java

--- CampoMinadoJogo/src/excecoes/OpcaoInvalidaException.java
+++ CampoMinadoJogo/src/excecoes/OpcaoInvalidaException.java
@@ -3,7 +3,7 @@ package excecoes;
 @SuppressWarnings("serial")
 public class OpcaoInvalidaException extends Exception {
 
-	public OpcaoInvalidaException(String mensagem) {
+	public OpcaoInvalidaException(String mensagem) { //excecao para opcao invalida no menu
 		super(mensagem);
 	}
 
