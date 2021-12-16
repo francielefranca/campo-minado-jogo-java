aa5c4bc916cdad46301f87e881340262a65b8f0f
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2021-12-16 14:07:47 -0300

    Update TamanhoInvalidoException.java

--- CampoMinadoJogo/src/excecoes/TamanhoInvalidoException.java
+++ CampoMinadoJogo/src/excecoes/TamanhoInvalidoException.java
@@ -3,7 +3,7 @@ package excecoes;
 @SuppressWarnings("serial")
 public class TamanhoInvalidoException extends Exception {
 
-	public TamanhoInvalidoException(String mensagem) {
+	public TamanhoInvalidoException(String mensagem) { //excecao para tamanho invalido de linha ou coluna entradas pelo jogador
 		super(mensagem);
 	}
 
