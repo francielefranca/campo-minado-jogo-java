d133dace8658191ba81e986a90a55eb423741bef
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2021-12-16 14:05:01 -0300

    Update AcaoInvalidaException.java

--- CampoMinadoJogo/src/excecoes/AcaoInvalidaException.java
+++ CampoMinadoJogo/src/excecoes/AcaoInvalidaException.java
@@ -3,7 +3,7 @@ package excecoes;
 @SuppressWarnings("serial")
 public class AcaoInvalidaException extends Exception {
 
-	public AcaoInvalidaException(String mensagem) {
+	public AcaoInvalidaException(String mensagem) { //excecao para acoes invalidas no meio do jogo
 		super(mensagem);
 	}
 
