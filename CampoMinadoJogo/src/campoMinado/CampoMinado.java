aea382446f19b874846a6a3ad949fa44ffb08906
brunoRobs <93611908+brunoRobs@users.noreply.github.com>
2021-12-16 13:53:58 -0300

    Update CampoMinado.java

--- CampoMinadoJogo/src/campoMinado/CampoMinado.java
+++ CampoMinadoJogo/src/campoMinado/CampoMinado.java
@@ -3,7 +3,7 @@ package campoMinado;
 import excecoes.OpcaoInvalidaException;
 
 public class CampoMinado {
-	public static void main(String[] args) throws OpcaoInvalidaException {
+	public static void main(String[] args) throws OpcaoInvalidaException { //execucao do programa
 		new Menu();
 	}
 }
